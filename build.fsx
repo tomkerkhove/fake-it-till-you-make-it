7d5e10713d80722fbe8f79d6e3a0a0ff4f03ed22
Tom Kerkhove <kerkhove.tom@gmail.com>
2023-04-04 22:07:51 +0200

    Working build scenario
    Signed-off-by: Tom Kerkhove <kerkhove.tom@gmail.com>

--- build.fsx
+++ build.fsx
@@ -1,13 +1,41 @@
 #r "paket:
-nuget Fake.Core.Target //"
+nuget Fake.IO.FileSystem ~> 5.23.1
+nuget Fake.DotNet.MSBuild ~> 5.23.1
+nuget Microsoft.Build 17.3.2
+nuget Microsoft.Build.Framework 17.3.2
+nuget Microsoft.Build.Tasks.Core 17.3.2
+nuget Microsoft.Build.Utilities.Core 17.3.2
+nuget Fake.Core.Target ~> 5.23.1 //"
 #load "./.fake/build.fsx/intellisense.fsx"
 
+open Fake.IO
+open Fake.IO.Globbing.Operators //enables !! and globbing
+open Fake.DotNet
 open Fake.Core
 
-// Default target
+// Properties
+let buildDir = "./build/"
+
+// Targets
+Target.create "Clean" (fun _ ->
+  Shell.cleanDir buildDir
+)
+
+Target.create "BuildApp" (fun _ ->
+  !! "src/**/*.csproj"
+    |> MSBuild.runRelease id buildDir "Build"
+    |> Trace.logItems "AppBuild-Output: "
+)
+
 Target.create "Default" (fun _ ->
   Trace.trace "Hello World from FAKE"
 )
+
+open Fake.Core.TargetOperators
+
+"Clean"
+  ==> "BuildApp"
+  ==> "Default"
 
 // start build
 Target.runOrDefault "Default"
