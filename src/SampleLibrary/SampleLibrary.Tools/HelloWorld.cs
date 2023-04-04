048afd7d862740604ea227d3abe55bb59ae07f42
Tom Kerkhove <kerkhove.tom@gmail.com>
2023-04-04 21:51:41 +0200

    Sample project
    Signed-off-by: Tom Kerkhove <kerkhove.tom@gmail.com>

---
+++ src/SampleLibrary/SampleLibrary.Tools/HelloWorld.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SampleLibrary.Tools
+{
+    public class HelloWorld
+    {
+        public static string WriteHi(string name)
+        {
+            return $"Hello World, from {name}";
+        }
+    }
+}
